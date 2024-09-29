bb8b1886e7f3920554a8d102f994b39aeac6cc72
Sir Nik <vikfed3717@gmail.com>
2024-09-29 23:01:41 +0300

    Task0

---
+++ Tyuiu.FedotkinNE.Sprint1.Task0.V2.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FedotkinNE.Sprint1.Task0.V2.Lib
+{
+    public class Class1
+    {
+    }
+}
