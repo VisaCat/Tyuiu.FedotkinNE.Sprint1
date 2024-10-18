f14ca497a595f95035e4f60fdd70170bd5eeaf63
Sir Nik <vikfed3717@gmail.com>
2024-10-18 11:56:43 +0300

    Task1

--- Tyuiu.FedotkinNE.Sprint1.Task1.V26/Program.cs
+++ Tyuiu.FedotkinNE.Sprint1.Task1.V26/Program.cs
@@ -1,9 +1,4 @@
-﻿
-
-
-
-using System;
-using Tyuiu.FedotkinNE.Sprint1.Task0.V2.Lib;
+﻿using Tyuiu.FedotkinNE.Sprint1.Task1.V26.Lib;
 
 namespace Tyuiu.FedotkinNE.Sprint1.Task0.V2
 {
@@ -14,7 +9,7 @@ namespace Tyuiu.FedotkinNE.Sprint1.Task0.V2
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Задание 0                                                               *");
+            Console.WriteLine("* Задание 1                                                               *");
             Console.WriteLine("* Выполнил: Федоткин Никита Евгеньевич | РППб-24-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
@@ -23,16 +18,20 @@ namespace Tyuiu.FedotkinNE.Sprint1.Task0.V2
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("3 / 3 * 2 + 12 / 4 / 3 - 2");
+            Console.WriteLine("(5*x) / (2+y)                                                              ");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
             DataService ds = new DataService();
-            Console.WriteLine(ds.Calculate());
+            double x, y;
+            Console.WriteLine("Введите значения x и y: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(ds.Calculate(x, y));
 
             Console.ReadKey();
 
-            
+
         }
     }
 }
